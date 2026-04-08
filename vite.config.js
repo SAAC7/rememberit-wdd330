79d6aa9a92557a00b976fd1aee428cd9a2faab55
SAAC7 <saulabraham881@gmail.com>
2026-04-08 03:23:59 -0600

    i make some changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+
 export default defineConfig({
-  base: '/rememberit-wdd330/', // 🔥 IMPORTANTE
+  base: '/rememberit-wdd330/',
   root: 'src/',
   build: {
     outDir: '../dist',
+    emptyOutDir: true, // 🔥 AGREGA ESTO
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
